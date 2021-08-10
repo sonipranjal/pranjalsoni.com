e14584c4021fc87dd6d87d475462b90807eb7536
sonipranjal <pranjalsoni6256@gmail.com>
2021-08-10 22:50:53 +0530

    resolved

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -13,9 +13,7 @@ import initMiddleware from '../../lib/init-middleware';
 const cors = initMiddleware(
   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
   Cors({
-    // Only allow requests with GET, POST and OPTIONS
-    methods: ['POST', 'OPTIONS'],
-    preflightContinue: true,
+    origin: '*',
   })
 );
 
