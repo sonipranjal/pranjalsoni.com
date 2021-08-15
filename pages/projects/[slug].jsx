be51e335f9e5278d0633df1d69cca5cfde723467
sonipranjal <pranjalsoni6256@gmail.com>
2021-08-15 20:44:18 +0530

    deleted some more files

--- pages/projects/[slug].jsx
+++ pages/projects/[slug].jsx
@@ -2,11 +2,7 @@ import fs from 'fs';
 import matter from 'gray-matter';
 import { MDXRemote } from 'next-mdx-remote';
 import { serialize } from 'next-mdx-remote/serialize';
-import dynamic from 'next/dynamic';
-import Head from 'next/head';
-import Link from 'next/link';
 import path from 'path';
-import CustomLink from '@/components/CustomLink';
 import Layout from '@/components/Layout';
 import { projectFilePaths, PROJECTS_PATH } from '../../utils/mdxUtils';
 
@@ -14,14 +10,14 @@ import { projectFilePaths, PROJECTS_PATH } from '../../utils/mdxUtils';
 // Since the MDX files aren't loaded by webpack, they have no knowledge of how
 // to handle import statements. Instead, you must include components in scope
 // here.
-const components = {
-  a: CustomLink,
-  // It also works with dynamically-imported components, which is especially
-  // useful for conditionally loading components for certain routes.
-  // See the notes in README.md for more details.
-  InlineLine: dynamic(() => import('@/components/InlineLine')),
-  Head,
-};
+// const components = {
+//   a: CustomLink,
+//   // It also works with dynamically-imported components, which is especially
+//   // useful for conditionally loading components for certain routes.
+//   // See the notes in README.md for more details.
+//   InlineLine: dynamic(() => import('@/components/InlineLine')),
+//   Head,s
+// };
 
 export default function PostPage({ source, frontMatter }) {
   return (
