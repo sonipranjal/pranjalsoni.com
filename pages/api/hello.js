cbf30d999d2495ddedfa9f7446b1ccc6ed790a75
sonipranjal <pranjalsoni6256@gmail.com>
2021-08-15 20:27:51 +0530

    deleted unnecessary files

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,12 +1,4 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import axios from 'axios';
 export default function handler(req, res) {
-  const fetchPosts = async () => {
-    const res = await axios.get(
-      'https://pranjalsoni.herokuapp.com/ghost/api/v4/content/posts/?key=aaf6a34f53e659a3a2abe7048a'
-    );
-    console.log(res);
-  };
-  fetchPosts();
-  res.status(200).json({ name: 'John Doe' });
+  res.status(200).json('Hello world');
 }
