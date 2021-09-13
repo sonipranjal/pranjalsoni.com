e82ebaed2223342a85f6044809a4f1bdd963e71f
sonipranjal <pranjalsoni6256@gmail.com>
2021-09-13 12:53:11 +0530

    updated resume

--- pages/resume/index.js
+++ pages/resume/index.js
@@ -5,7 +5,7 @@ export default function Resume() {
     <Layout>
       <div className="flex items-center justify-center w-full mx-5 my-10 h-[450px] md:h-screen xl:h-[1000px]">
         <iframe
-          src="https://drive.google.com/file/d/1uzzaWn-sVZsyAc_vkkopIrc8ZQMF8bXm/preview"
+          src="https://drive.google.com/file/d/1SoWDP4NeRJQxpxyj9fiaDZhji78aMAxp/preview"
           className="w-full h-full md:w-10/12"
           allow="autoplay"
         ></iframe>
